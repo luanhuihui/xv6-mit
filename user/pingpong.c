d778e58e0d900a861634a1e2b3a3516bc5ab406a
luan <woshiluanhuihui@gmail.com>
2025-02-19 21:50:34 +0800

    001

--- user/pingpong.c
+++ user/pingpong.c
@@ -4,31 +4,29 @@
 
 
 int main(int argc, char * argv[]){
-    int fd[2];
-    pipe(fd);//先创建管道
+    int p[2];
+    pipe(p);
     char buf[5];
     int pid = fork();
     if(pid == 0){
-        read(fd[0], buf, sizeof buf);
-        close(fd[0]);
-        printf("%d: received %s\n", pid, buf);
-        write(fd[1], "pong", 5);
-
-        close(fd[1]);
+        read(p[0], buf, sizeof buf);
+        close(p[0]);
+        printf("%d: received  %s\n", pid, buf);
+        write(p[1], "pong", 5);
+        close(p[1]);
         exit(0);
     }
     else if(pid > 0){
-        write(fd[1], "ping", 4);
-        close(fd[1]);
-
-        read(fd[0], buf, sizeof buf);
-        close(fd[0]);
+        write(p[1], "ping", 5);
+        close(p[1]);
+        wait(0);
+        read(p[0], buf, sizeof buf);
+        close(p[0]);
         printf("%d: received %s\n", pid, buf);
         exit(0);
     }
-    else {
+    else{
         exit(1);
     }
     return 0;
-
 }
